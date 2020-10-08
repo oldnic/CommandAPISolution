2d45220293d1aa2eb6c705a14d8fb8cda55a6c39
oldnic <zaid.alaggel@gmail.com>
2020-10-08 21:38:28 +0300

    Added Model and Mock Repository

--- CommandAPI/Controllers/CommandsController.cs
+++ CommandAPI/Controllers/CommandsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CommandAPI.Data;
+using CommandAPI.Models;
 
 namespace CommandAPI.Controllers {
 
@@ -10,9 +12,31 @@ namespace CommandAPI.Controllers {
     [ApiController]
     public class CommandsController : ControllerBase {
 
-        [HttpGet]
+        private readonly ICommandAPIRepo _repository;
+
+        public CommandsController(ICommandAPIRepo repository) {
+            _repository = repository;
+        }
+
+        /*[HttpGet]
         public ActionResult<IEnumerable<String>> Get() {
             return new string[] { "this", "is", "hard", "coded" };
+        }*/
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands() {
+            IEnumerable<Command> commandItems = _repository.GetAllCommands();
+            return Ok(commandItems);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id) {
+            Command commandItem = _repository.GetCommandById(id);
+
+            if (commandItem == null) {
+                return NotFound();
+            }
+            return Ok(commandItem);
         }
     }
 }
--- CommandAPI/Startup.cs
+++ CommandAPI/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CommandAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -14,6 +15,7 @@ namespace CommandAPI {
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
             services.AddControllers();
+            services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
