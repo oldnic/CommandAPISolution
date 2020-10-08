28135106a7a239cfe5b92d9754cba505daac8203
oldnic <zaid.alaggel@gmail.com>
2020-10-08 17:03:00 +0300

    Add project files.

---
+++ CommandAPI/Controllers/CommandsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandAPI.Controllers {
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase {
+
+        [HttpGet]
+        public ActionResult<IEnumerable<String>> Get() {
+            return new string[] { "this", "is", "hard", "coded" };
+        }
+    }
+}
